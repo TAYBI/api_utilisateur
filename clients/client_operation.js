52493464bbf67720b7d9e6398308db3d44b56a31
Bilal <taybib4@gmail.com>
2023-06-05 17:50:25 +0100

    add client

--- clients/client_operation.js
+++ clients/client_operation.js
@@ -1,19 +1,13 @@
 var config = require('../dbconfig');
 const sql = require('mssql/msnodesqlv8');
 
-
-
 async function getClients(database) {
     try {
         let pool = await sql.connect(config);
-        // var cmd = `select code_client, nom, adresse, tel, ville.libelle ville
-        //     from client
-        //     left join Ville on client.IDville=Ville.IDville`;
-        var cmd = `select code_client, nom, adresse, tel, IDvill
-            from [${database}].dbo.client`;
+        cmd = `select code_client, nom, adresse, tel, IDvill
+        from [${database}].dbo.client`;
 
         console.log(cmd);
-        console.log(database);
         let result = pool.request().query(cmd);
         return (await result).recordset
     }
@@ -22,7 +16,23 @@ async function getClients(database) {
     }
 }
 
+async function addClient(database, client) {
+    try {
+        let pool = await sql.connect(config);
+        var cmd = `insert into [${database}].dbo.client (code_client, nom, adresse, tel, IdVill) 
+                values  ('${client.code_client}', '${client.Nom}', '${client.Adresse}', '${client.Telephone}', ${client.IdVill})`;
+        console.log(cmd);
+        let result = pool.request().query(cmd);
+
+        return (await result).recordset
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
+
+
 module.exports = {
     getClients: getClients,
-
+    addClient: addClient
 }
--- clients/clients.js
+++ clients/clients.js
@@ -1,13 +1,25 @@
 const express = require('express');
 const router = express.Router();
 const dboperations = require('./client_operation');
+const { database } = require('../dbconfig');
+
 
 // lecture
-router.route('/:database/clients').get((request, response) => {
-    dboperations.getClients(request.params.database).then(result => {
+router.get('/:database/clients', (req, res) => {
+    dboperations.getClients(req.params.database.toString().replace('@', ' ')).then((result) => {
+        res.json(result);
+    });
+});
+
+
+//ajoute
+router.post('/:database/client', (request, response) => {
+    let client = { ...request.body }
+    console.log(client);
+
+    dboperations.addClient(request.params.database.toString().replace('@', ' '), client).then(result => {
         response.json(result);
     })
 })
-
 
 module.exports = router;
